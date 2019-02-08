03884b60eb6a6b423d14372c72c5e430642363b7
mauro calderon <caldem11@gmatc.matc.edu>
2019-02-07 20:59:23 -0600

    create element Components

---
+++ components/src/components/Element/Button/Button.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Proptypes from 'prop-types'
+import {ButtonBase} from './ButtonBase'
+import styled from 'styled-components'
+
+const StyledButton = styled(ButtonBase)`
+    padding: 8px;
+    color: ${props =>(props.variant === 'primary'?'black':'white')};
+    border: solid 1px
+    ${props =>(props.variant === 'primary'?'black':'white')};
+
+`
+
+export const Button = props => <StyledButton {...props}/>
+
+Button.prototype={
+    variant: Proptypes.string
+}
--- components/src/pages/index.js
+++ components/src/pages/index.js
@@ -4,6 +4,7 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import {Address, Button} from "../components/Element"
 
 const IndexPage = () => (
   <Layout>
@@ -14,7 +15,10 @@ const IndexPage = () => (
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Address>
+    My Address is 123 Sleepy
+    <Button variant="primary">Contact Me</Button>
+    </Address>
   </Layout>
 )
 
