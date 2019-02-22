41e2eece4eb9c0ef2fa7191c5af32d864d04f778
mauro calderon <caldem11@gmatc.matc.edu>
2019-02-21 20:16:33 -0600

    Wrapped Link in styled component in the Masthead component

--- components/src/components/Masthead/Masthead.js
+++ components/src/components/Masthead/Masthead.js
@@ -11,18 +11,14 @@ background-color: rebaccapurple;
 margin-button: 1.45rem;
 
 `
+const StyledLink = styled(Link)`
+color: white;
+text-decoration: none
+`
 export const Masthead =({height,siteTitle}) => {
     return(
         <Outer height={height}>
-        <Link 
-        href="/"
-        style={{
-            color:`white`,
-            textDecoration:`none`
-        }}
-        >
-        {siteTitle}
-        </Link>
+        <StyledLink href="/">{siteTitle}</StyledLink>
         </Outer>
     )
 }
